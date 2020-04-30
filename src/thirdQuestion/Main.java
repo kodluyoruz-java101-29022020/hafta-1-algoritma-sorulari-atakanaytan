92214d72ad91754aa1063d38d129a9a24b495cf2
atakanaytan <aytan.atakan2@gmail.com>
2020-05-01 02:37:57 +0300

    Third question was added

---
+++ src/thirdQuestion/Main.java
@@ -0,0 +1,44 @@
+package thirdQuestion;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] example = fillArray();
+
+        double result = sumArray(example);
+
+        System.out.println("res: "+result);
+    }
+
+
+    public static int[] fillArray(){
+
+        Random random = new Random();
+        int[] randomNumbersList = new int[100];
+
+        for (int i=0; i<randomNumbersList.length; i++){
+
+            randomNumbersList[i] = random.nextInt(100);
+            System.out.println("i: " + randomNumbersList[i]);
+        }
+
+        return randomNumbersList;
+    }
+
+    public static double sumArray(int[] array) {
+
+        int sum = 0;
+
+        for (int i=0; i<array.length; i++) {
+            sum += array[i];
+        }
+
+        double result = sum / array.length;
+
+        return result;
+    }
+
+}
